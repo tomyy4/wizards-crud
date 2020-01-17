96a02de62663afbaca5e5bc6e86d952893779182
tomeu <tomeullompartpiza@gmail.com>
2020-01-17 16:53:24 +0100

    app context

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
@@ -18,14 +21,15 @@ def index():
 def wizards():
     service = WizardService(WizardRepository)
     wizards = service.get_all_wizards()
-    return [w.name for w in wizards]
+    wizard_names = {w.name for w in wizards}
+    return 'wizards'
 
 
 @app.route('/houses')
 def houses():
     service = HouseService(HouseRepository)
     houses = service.get_all_houses()
-    return [h.name for h in houses]
+    return 'houses'
 
 
 if __name__ == '__main__':
