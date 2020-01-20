606295fbbac3ac3892cdb953bea9cfa3e3521ed2
tomeu <tomeullompartpiza@gmail.com>
2020-01-20 17:32:20 +0100

    tests updated

--- models/base_models.py
+++ models/base_models.py
@@ -1,17 +1,13 @@
 
 
 class Wizard:
-    def __init__(self, name, age, has_received_letter, dark_wizard, voldemort_friend, house_id):
+    def __init__(self, name, age, house_id):
         self.name = name
         self.age = age
-        self.has_received_letter = has_received_letter
-        self.dark_wizard = dark_wizard
-        self.voldemort_friend = voldemort_friend
         self.house_id = house_id
 
 
 class House:
-    def __init__(self, name, max_students, teaches_dark_arts):
+    def __init__(self, name, max_students):
         self.name = name
         self.students = max_students
-        self.teaches_dark_arts = teaches_dark_arts
--- tests.py
+++ tests.py
@@ -7,72 +7,89 @@ from repositories.repositories import BaseWizardRepository, BaseHouseRepository
 
 class CreateWizardTest(unittest.TestCase):
     def test_wizard_is_created_with_right_parameters(self):
-        w = Wizard('Harry',17, 1, 0, 0, 1)
-
+        w = Wizard('Harry',17, 1)
+        has_received_letter = 1
+        wont_turn_into_dark_wizard = 0
+        is_not_voldemort_friend = 0
         b = BaseWizardRepository()
         service = WizardService(b)
         create_wizard = service.create_wizard(
-            w.name, w.age, w.has_received_letter, w.dark_wizard, w.voldemort_friend, w.house_id
+            w.name, w.age, has_received_letter, wont_turn_into_dark_wizard,is_not_voldemort_friend, w.house_id
         )
         self.assertTrue(create_wizard)
 
     def test_cannot_create_wizard_older_than_18_wizard(self):
-        w = Wizard('Harry', 19, 1, 0, 0, 1)
+        w = Wizard('Harry', 19, 1)
+        has_received_letter = 1
+        wont_turn_into_dark_wizard = 0
+        is_not_voldemort_friend = 0
         b = BaseWizardRepository()
         service = WizardService(b)
         create_wizard = service.create_wizard(
-            w.name, w.age, w.has_received_letter, w.dark_wizard, w.voldemort_friend, w.house_id
+            w.name, w.age, has_received_letter, wont_turn_into_dark_wizard, is_not_voldemort_friend, w.house_id
         )
         self.assertFalse(create_wizard)
 
     def test_cannot_register_if_has_not_received_letter(self):
-        w = Wizard('Harry', 19, 0, 0, 0, 1)
+        w = Wizard('Harry', 15, 1)
         b = BaseWizardRepository()
         service = WizardService(b)
+        has_received_letter = 0
+        wont_turn_into_dark_wizard = 0
+        is_not_voldemort_friend = 0
         create_wizard = service.create_wizard(
-            w.name, w.age, w.has_received_letter, w.dark_wizard, w.voldemort_friend, w.house_id
+            w.name, w.age, has_received_letter, wont_turn_into_dark_wizard, is_not_voldemort_friend, w.house_id
         )
         self.assertFalse(create_wizard)
 
     def test_cannot_register_if_will_turn_into_dark_mage(self):
-        w = Wizard('Harry', 15, 1, 1, 0, 1)
+        w = Wizard('Harry', 15, 1)
         b = BaseWizardRepository()
         service = WizardService(b)
+        has_received_letter = 1
+        wont_turn_into_dark_wizard = 1
+        is_not_voldemort_friend = 0
         create_wizard = service.create_wizard(
-            w.name, w.age, w.has_received_letter, w.dark_wizard, w.voldemort_friend, w.house_id
+            w.name, w.age, has_received_letter, wont_turn_into_dark_wizard, is_not_voldemort_friend, w.house_id
         )
         self.assertFalse(create_wizard)
 
     def test_cannot_register_if_is_friend_with_voldemort(self):
-        w = Wizard('Harry', 15, 1, 0, 1, 1)
+        w = Wizard('Harry', 15, 1)
+        has_received_letter = 1
+        wont_turn_into_dark_wizard = 0
+        is_not_voldemort_friend = 1
         b = BaseWizardRepository()
         service = WizardService(b)
         create_wizard = service.create_wizard(
-            w.name, w.age, w.has_received_letter, w.dark_wizard, w.voldemort_friend, w.house_id
+            w.name, w.age, has_received_letter, wont_turn_into_dark_wizard, is_not_voldemort_friend, w.house_id
         )
         self.assertFalse(create_wizard)
 
 
 class CreateHouseTest(unittest.TestCase):
     def test_can_create_house_with_right_parameters(self):
-        h = House('Griffindor', 50, 0)
+        h = House('Griffindor', 50)
+        teaches_dark_arts = 0
         repo = BaseHouseRepository()
         service = HouseService(repo)
-        create_house = service.create_house(h.name, h.students, h.students)
-        self.assertFalse(create_house)
+        create_house = service.create_house(h.name, h.students, teaches_dark_arts)
+        self.assertTrue(create_house)
 
     def test_cannot_create_house_if_teaches_dark_arts(self):
-        h = House('Griffindor', 50, 1)
+        h = House('Griffindor', 50)
+        teaches_dark_arts = 1
         repo = BaseHouseRepository()
         service = HouseService(repo)
-        create_house = service.create_house(h.name, h.students, h.students)
+        create_house = service.create_house(h.name, h.students, teaches_dark_arts)
         self.assertFalse(create_house)
 
     def test_house_cannot_have_more_thant_50_students(self):
-        h = House('Griffindor', 51, 1)
+        h = House('Griffindor', 51)
+        teaches_dark_arts = 0
         repo = BaseHouseRepository()
         service = HouseService(repo)
-        create_house = service.create_house(h.name, h.students, h.students)
+        create_house = service.create_house(h.name, h.students, teaches_dark_arts)
         self.assertFalse(create_house)
 
 
